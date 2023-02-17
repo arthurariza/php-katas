5d2b2db68900d49a194a238a8a65eb5241687771
arthurariza <tucaariza@gmail.com>
2023-02-17 13:18:32 -0300

    Sample Test Case

---
+++ tests/Test.php
@@ -0,0 +1,17 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function testAssertTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testAssertFalse()
+    {
+        $this->assertTrue(true);
+    }
+}
